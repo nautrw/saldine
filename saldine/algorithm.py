3e26eabbd4ab574c11f5570772afced076147a7c
nautrw <nautrw@proton.me>
2024-07-02 11:57:16 -0400

    fix: fix many things - use random.choices instead of random.choice - use the go up or down arg in the main function - add type hinting

--- saldine/algorithm.py
+++ saldine/algorithm.py
@@ -1,4 +1,5 @@
 import random
+from typing import List
 
 NOTES = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G"}
 
@@ -20,8 +21,8 @@ NOTE_COLORS = {
 }
 
 
-def interval(note_number: int, go_down: bool):
-    interval = random.choice(INTERVALS, weights=INTERVAL_WEIGHTS)
+def interval(note_number: int, go_down: bool) -> int:
+    interval = random.choices(INTERVALS, weights=INTERVAL_WEIGHTS)[0]
 
     interval = -interval if go_down else interval
     note_number += interval
@@ -29,11 +30,11 @@ def interval(note_number: int, go_down: bool):
     return (note_number % 7) or 7
 
 
-def note_length():
-    return random.choice(NOTE_LENGTHS, weights=NOTE_LENGTH_WEIGHTS)
+def note_length() -> str:
+    return random.choices(NOTE_LENGTHS, weights=NOTE_LENGTH_WEIGHTS)[0]
 
 
-def generate_notes_list(notes_number: int, add_whole_note_at_end: bool):
+def generate_notes_list(notes_number: int, add_whole_note_at_end: bool) -> List[dict]:
     if notes_number < 2:
         exit("The algorithm can only generate with 3 or more notes.")
 
@@ -42,21 +43,28 @@ def generate_notes_list(notes_number: int, add_whole_note_at_end: bool):
 
     notes_number -= 1  # accommodating for the beginning note
 
-    notes_list = [{"number": interval(random.randint(1, 7)), "length": note_length()}]
+    notes_list = [
+        {
+            "number": interval(random.randint(1, 7), random.choice([True, False])),
+            "length": note_length(),
+        }
+    ]
 
     for _ in range(notes_number):
         previous_note = notes_list[-1]
 
         notes_list.append(
             {
-                "number": interval(previous_note["number"]),
+                "number": interval(
+                    previous_note["number"], random.choice([True, False])
+                ),
                 "length": note_length(),
             }
         )
 
     if add_whole_note_at_end:
         new_note_dict = {
-            "number": interval(notes_list[-1]["number"]),
+            "number": interval(notes_list[-1]["number"], random.choice([True, False])),
             "length": "whole",
         }
         notes_list.append(new_note_dict)
